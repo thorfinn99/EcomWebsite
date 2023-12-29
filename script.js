b805ca7f0853bd13344c94e48571d625be704b67
thorfinn99 <146380903+thorfinn99@users.noreply.github.com>
2023-12-29 18:05:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+const mainImg = document.getElementById("main-img")
+
+const smolImg1 = document.getElementById("smol-img1")
+const smolImg2 = document.getElementById("smol-img2")
+const smolImg3 = document.getElementById("smol-img3")
+const smolImg4 = document.getElementById("smol-img4")
+
+const smolImg5 = document.getElementById("smol-img5")
+const smolImg6 = document.getElementById("smol-img6")
+const smolImg7 = document.getElementById("smol-img7")
+const smolImg8 = document.getElementById("smol-img8")
+
+function swap1(){
+    mainImg.src = "f1.jpg"
+}
+function swap2(){
+    mainImg.src = "f2.jpg"
+}
+function swap3(){
+    mainImg.src = "f3.jpg"
+}
+function swap4(){
+    mainImg.src = "f4.jpg"
+}
+
+function swap5(){
+    mainImg.src = "f5.jpg"
+}
+function swap6(){
+    mainImg.src = "f6.jpg"
+}
+function swap7(){
+    mainImg.src = "f7.jpg"
+}
+function swap8(){
+    mainImg.src = "f8.jpg"
+}
